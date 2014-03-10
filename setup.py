38f98718d8011437d5f276f9080a8ebbf1348195
VeryCB <imcaibin@gmail.com>
2014-03-10 18:08:26 +0800

    ready to publish

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+from jos import __version__
+
+
+with open('README.rst') as long_description_file:
+    long_description = long_description_file.read()
+
+
+setup(
+    name='jos',
+    version=__version__,
+    url='https://github.com/verycb/jos',
+    author='VeryCB',
+    author_email='imcaibin@gmail.com',
+    description='Jingdong API Client',
+    long_description=long_description,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
